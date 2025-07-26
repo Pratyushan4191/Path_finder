8e0917007a049095ef464ee916870a7a07a533e9
Pratyushan4191 <pratyushansaidanta@gmail.com>
2025-07-26 11:14:15 +0530

    fixed case sensetive in vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Path_Finder/', // <-- IMPORTANT for static hosting or opening directly
+  base: '/Path_finder/', // <-- IMPORTANT for static hosting or opening directly
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
