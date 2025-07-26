a7f266b6b2344d48fb8e7791e99b409c7fa545c6
Pratyushan4191 <pratyushansaidanta@gmail.com>
2025-07-26 11:07:49 +0530

    updated the project before deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Path_Finder/', // <-- IMPORTANT for static hosting or opening directly
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
